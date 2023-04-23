9f826cf93b0c27d47fd062f7324daf3b4dece867
Alistair Fraser <afraser@lcs.on.ca>
2023-04-22 20:45:46 -0400

    Initial Commit

---
+++ AdviceFinder/AdviceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdviceFinderApp.swift
+//  AdviceFinder
+//
+//  Created by Alistair Fraser on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct AdviceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
